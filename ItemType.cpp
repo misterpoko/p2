a758af333640be617022eb68fb379047dc6ad8f1
Nathaniel <nbm54197@uga.edu>
2021-09-28 00:03:25 -0400

    made prototype for itemtype.cpp

---
+++ ItemType.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "ItemType.h"
+
+
+int value = NULL;
+//public:
+enum Comparison {GREATER, LESS, EQUAL};
+/**
+ * This method compares the item given to the item of this specific instance
+ * If the item that is given is greater than the one of this instance then it will
+ * return greater or less for less and equal if they are equal.
+ * @param item the item being compared
+ * @return an enum if its greater less or equal
+ */	
+ItemType::Comparison ItemType::compareTo(ItemType item)
+{
+	if (item.getValue() > value)
+	{
+		return GREATER;
+	}
+	else if (item.getValue() == value)
+	{
+		return EQUAL;	
+	}
+	else
+	{
+		return LESS;
+	}
+} // compareTo
+
+/**
+ * This method gets the value of the ItemType instance
+ * @return the value of the ItemType
+ */
+int ItemType::getValue() const
+{
+	return value;
+} // getValue
+
+/**
+ * This is how we put a value into an ItemType()
+ * @param num the number that is going to be put into itemtype
+ */	
+void ItemType::initialize(int num)
+{
+	value = num;
+} // initialize
+
+//private:
